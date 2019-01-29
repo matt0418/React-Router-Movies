8da934821c0110ca2d94021e7edeae76a09dd07b
Matt Feldman <mfeldma7@gmail.com>
2019-01-29 18:56:47 -0500

    Pushing saves

--- client/src/App.js
+++ client/src/App.js
@@ -40,7 +40,6 @@ export default class App extends Component {
         // component={Movie} 
         render={props => <Movie {...props} addToSavedList={this.addToSavedList}/>}
         />
-        
       </div>
     );
   }
